096ff7c56c4e201de633b3d77a5fb68a6e4205cc
Irene Ockrah <ostrene33@gmail.com>
2022-07-17 03:15:39 -0700

    conditionals practice

---
+++ conditionals.js
@@ -0,0 +1,31 @@
+//exercise 1
+function trial(g, f, h){
+	console.log (g + f + h);
+
+}
+
+//exercise 2
+trial(10, 3, 5)
+
+function even (){
+	for (i=44; i<=70; i++){
+		if(i%2==0){
+			document.write(i + "<br>")
+		}
+	}
+}
+
+even()
+
+//exercise 3
+
+function bag () {
+	for (i=10; i<=20; i++){
+		if(20-i==5)
+		{
+			alert(i);
+		}
+	}
+}
+
+bag()
